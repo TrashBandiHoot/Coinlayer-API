50fd97121183a3041a77405976673feaf30377bc
TrashBandiHoot <112197685+TrashBandiHoot@users.noreply.github.com>
2022-11-14 17:21:02 -0600

    fixed syntax/readability

--- main.js
+++ main.js
@@ -80,7 +80,8 @@ function coinDataDisplay(data)
 }
 
 
-function myFunction() {
+function myFunction() 
+{
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
@@ -89,13 +90,18 @@ function myFunction() {
     tr = table.getElementsByTagName("tr");
   
     // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
+    for (i = 0; i < tr.length; i++) 
+    {
       td = tr[i].getElementsByTagName("td")[1];
-      if (td) {
+      if (td) 
+      {
         txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        if (txtValue.toUpperCase().indexOf(filter) > -1) 
+        {
           tr[i].style.display = "";
-        } else {
+        } 
+        else 
+        {
           tr[i].style.display = "none";
         }
       }
